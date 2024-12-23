9c5313e34c19b85a01e5d4abe11a06cd84049564
Aushev <aushevimran198@gmail.com>
2024-12-23 17:36:10 +0300

    added logo.svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Logo from "../assets/img/logo.jpg";
+import Logo from "../assets/img/logo.svg";
+import Telegram from "../assets/img/telegram.svg";
 // import { useState } from "react";
 
 // const [state, setState] = useState(0);
@@ -14,18 +15,32 @@ const Header = () => {
       <div className="header__logo">
         <img src={Logo} alt="Logo" />
       </div>
-      <div className="header__menu">
-        <ul>
+      <div className="header-menu">
+        <ul className="header-menu__list">
           {list.map((value) => {
             return (
-              <li>
-                <a href="/#">{value}</a>
+              <li className="header-menu__item">
+                <a className="header-menu__link" href="/#">
+                  {value}
+                </a>
               </li>
             );
           })}
         </ul>
       </div>
-      <div className="header-menu__additional-wrap"></div>
+      <div className="header-menu__additional-wrap">
+        <div className="header-menu__additional">
+          <a className="header-menu__tg" href="tg://resolve?domain=awg_ru">
+            <img src={Telegram} />
+          </a>
+          <a className="header-menu__phone" href="tel:+74952780708">
+            +7 965 888-06-06{" "}
+          </a>
+          <a className="header-menu__lang" href="/en/">
+            Eng
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
