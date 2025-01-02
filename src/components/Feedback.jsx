1a76704e28c1935f2e6a82894cf2bffd75c82c50
Coder <aushevimran198@gmail.com>
2025-01-02 04:50:56 +0300

    add button feedback

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Feedback = () => {
+  return (
+    <div className="feedback">
+      <button class="apply-button">Оставить заявку</button>
+    </div>
+  );
+};
+
+export default Feedback;
